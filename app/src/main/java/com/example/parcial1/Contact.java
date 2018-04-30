64b4724f1b06a27db9370c974dc7b9d49c8bb76d
emartinezs <00116316@uca.edu.sv>
2018-04-29 18:12:58 -0600

    Clase modelo de un contacto

---
+++ app/src/main/java/com/example/parcial1/Contact.java
@@ -0,0 +1,66 @@
+package com.example.parcial1;
+
+public class Contact {
+    private String name;
+    private String lastName;
+    private int id;
+    private String email;
+    private String address;
+    private boolean favorite = false;
+
+    public Contact(String name, String lastName, int id, String email, String address) {
+        this.name = name;
+        this.lastName = lastName;
+        this.id = id;
+        this.email = email;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public boolean isFavorite() {
+        return favorite;
+    }
+
+    public void setFavorite(boolean favorite) {
+        this.favorite = favorite;
+    }
+}
